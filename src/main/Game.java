2e6eb14b7d8073197b68d4664c08f77ac3fff096
liviuxyz-ctrl <lviu2002liviu@gmail.com>
2021-10-13 18:52:39 +0300

    First Commit

---
+++ src/main/Game.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+
+public class Game extends JFrame {
+    //Constructor
+    public Game(){
+        setSize(400,400);
+
+    }
+    public static void main(String[] args) {
+
+        Game game = new Game();
+    }
+}
