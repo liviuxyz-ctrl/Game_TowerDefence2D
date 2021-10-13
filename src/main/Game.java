2aae9ae918fc653d372d554c2f122d466413ab80
liviuxyz-ctrl <lviu2002liviu@gmail.com>
2021-10-13 19:21:43 +0300

    Basic drawing

--- src/main/Game.java
+++ src/main/Game.java
@@ -3,9 +3,17 @@ package main;
 import javax.swing.*;
 
 public class Game extends JFrame {
+
+    private GameScreen gameScreen;
+
     //Constructor
     public Game(){
         setSize(400,400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        gameScreen = new GameScreen();
+        add(gameScreen);
 
     }
     public static void main(String[] args) {
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -1,4 +1,17 @@
 package main;
 
-public class GameScreen {
+import javax.swing.*;
+import java.awt.*;
+
+public class GameScreen extends JPanel {
+    public GameScreen(){
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.drawRect(50,50,100,100);
+
+    }
 }
